24d0e1a7c2557ed51c6fb6095a0e31feb2f078b6
jenndalt@gmail.com <jenndalt@gmail.com>
2019-08-01 18:42:09 -0500

    adding tax model

--- calculation-microservice/src/main/java/com/trilogy/calculationmicroservice/model/Tax.java
+++ calculation-microservice/src/main/java/com/trilogy/calculationmicroservice/model/Tax.java
@@ -1,4 +1,58 @@
 package com.trilogy.calculationmicroservice.model;
 
+import java.util.Objects;
+
 public class Tax {
+    private Integer categoryId;
+    private String category;
+    private Double taxPercent;
+    private Boolean taxExempt;
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getTaxPercent() {
+        return taxPercent;
+    }
+
+    public void setTaxPercent(Double taxPercent) {
+        this.taxPercent = taxPercent;
+    }
+
+    public Boolean getTaxExempt() {
+        return taxExempt;
+    }
+
+    public void setTaxExempt(Boolean taxExempt) {
+        this.taxExempt = taxExempt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tax tax = (Tax) o;
+        return Objects.equals(categoryId, tax.categoryId) &&
+                Objects.equals(category, tax.category) &&
+                Objects.equals(taxPercent, tax.taxPercent) &&
+                Objects.equals(taxExempt, tax.taxExempt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(categoryId, category, taxPercent, taxExempt);
+    }
 }
